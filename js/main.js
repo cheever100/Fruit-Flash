ef1aa7d81aae8ee5140b0d39f3a1ccb6c160e271
Jean Cheever <cheever100@gmail.com>
2016-08-23 16:12:24 -0400

    added question 3 with fruit and code

--- js/main.js
+++ js/main.js
@@ -1,18 +1,39 @@
-var answers = {q1:"raspberries"}
+var answers = {q1:"raspberries", q2:"papaya", q3:"pomegranates"}
 
 $(document).ready(function(){
-        $("input[type='button']").click(function(){
-        	var radioValue = $("input[name='berry']:checked").val();
-            
-            if( radioValue == answers.q1 ) {
-    
-            	$("#answer_box").html("Good job!");
-            } else {
-            	
-            	$("#answer_box").html("Sorry, try again!");
-            }
-        });
-        
+
+    // slide bar for helpful hints
+    // $("#flip").click(function(){
+    //     $("#panel").slideDown("slow");
+    // });
+
+// slide bar for helpful hints
+    $(".flip").click(function(){
+        $("#panel[name='"+this.getAttribute('name')+"']").slideDown("slow");
+        console.log(this.getAttribute('name'));
     });
+
+
+
+
+
+
+
+    // selecting answers
+    $("input[type='button']").click(function(){ 
+        var radio_selector = "input[name='"+this.id+"']:checked";
+        var radioValue = $(radio_selector).val();
+        var answer_box_selector = "#answer_box[name='"+this.id+"']";
+            
+        if( radioValue == answers[this.id] ) {
+           
+            $(answer_box_selector).html("Good job!");
+               
+        } else {	
+            	
+            $(answer_box_selector).html("Sorry, try again!");    
+        }
+    });  
+});
 
 
