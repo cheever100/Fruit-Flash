4af2495303274ec05af5b9e4e72e1e16aca91c3d
Jean Cheever <cheever100@gmail.com>
2016-08-25 15:14:01 -0400

    changed Stew Leonard image to add capitalization

--- js/demo.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-        $("input[type='button']").click(function(){
-        	var radioValue = $("input[name='berry']:checked").val();
-            if(radioValue){
-                alert("Your are " + radioValue);
-            }
-        });
-        
-    });
